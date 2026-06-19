f01628c21d75c095cd20676a3a6e9018223a8b96
anwar-afk <anwarfaizfauzi@gmail.com>
2026-06-19 15:03:42 +0700

    update version

--- migrations/initial.js
+++ migrations/initial.js
@@ -1,11 +1,22 @@
 const { sequelize } = require('../config/db');
 
+// Import all models
+require('../models/userModel');
+require('../models/campaignModel');
+require('../models/donationModel');
+require('../models/newsModel');
+require('../models/statisticModel');
+require('../models/documentationModel');
+require('../models/documentModel');
+
 async function migrate() {
   try {
     await sequelize.sync({ alter: true });
     console.log('Database synchronized');
+    process.exit(0);
   } catch (error) {
     console.error('Migration error:', error);
+    process.exit(1);
   }
 }
 
